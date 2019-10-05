4596a411b5f1df1f6762667fd59c8fe7aa5a1112
Виталий <wental-@mail.ru>
2019-10-05 05:11:52 +0300

    Регистрируемся на сайте

--- register.php
+++ register.php
@@ -11,23 +11,86 @@ $show_complete_tasks = rand(0, 1);
 $title = "Регистрация";
 $user_name = "Виталий";
 
+/******************************************************************
+ * 
+ * Внимание, для незалогиненого пользователя недолжно быть кнопки
+ * "Добавить задачу" в правом верхнем углу
+ * 
+ ******************************************************************/
 
+$new_user_email = "";
+$new_user_password = "";
+$new_user_name = "";
 
+//Сохранение информации из заполненных полей
+if (!empty($_POST["email"])) {
+    $new_user_email = htmlspecialchars(trim($_POST["email"]));
+}
+if (!empty($_POST["password"])) {
+    $new_user_password = htmlspecialchars(trim($_POST["password"]));
+}
+if (!empty($_POST["name"])) {
+    $new_user_name = htmlspecialchars(trim($_POST["name"]));
+}
 
+$required_fields = ["email", "password", "name"];
+$errors = [];
 
+//Проверка формы на отправленность
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    
+    //Проверка адреса электронной почты на валидность
+    if (!filter_var($new_user_email, FILTER_VALIDATE_EMAIL)) {
+        $errors["email"] = "E-mail введён некорректно";
+    }
+    
+    //Проверка на заполненность обязательных полей и их длину
+    foreach ($required_fields as $required_field) {
+        if (empty(trim($_POST[$required_field]))) {
+            $errors[$required_field] = "Поле обязательно для заполнения";
+        } elseif (strlen(trim($_POST[$required_field])) > 127) {
+            $errors[$required_field] = "Введена слишком длинная строка";
+        }
+    }
 
+    //Проверка на существование почтового адреса в БД  
+    if (empty($errors["email"])) {
+        $query_email = "SELECT id FROM users WHERE email = ?;";
+        $stmt_email = db_get_prepare_stmt($connect, $query_email, 
+                [$new_user_email]);
+        mysqli_stmt_execute($stmt_email);
+        $stmt_email_result = mysqli_stmt_get_result($stmt_email);
+        if (mysqli_num_rows($stmt_email_result)) {
+            $errors["email"] = "Пользователь с такой почтой уже существует";
+        }
+    }
+ 
+    //все проверки пройдены, сохраняем пользователя в БД
+    if (count($errors) === 0) {
+        //Создание хэша пароля для его хранения в БД
+        $pass_hash = password_hash($new_user_password, PASSWORD_DEFAULT);
+        
+        //Добавление в базу
+        $query_new_user = "INSERT INTO users (email, name, password) "
+                . "VALUES (?, ?, ?);";
+        $stmt_new_user = db_get_prepare_stmt($connect, $query_new_user, 
+                [$new_user_email, 
+                 $new_user_name, 
+                 $pass_hash]);
+        mysqli_stmt_execute($stmt_new_user);
 
-
-
-
+        //редирект на главную
+        header("Location: /index.php");
+    }
+}
 
 $content = include_template("register_tmp.php", [
-    "projects"         => $projects,
-    "tasks_list"       => $tasks_list,
-    //"new_task_name"    => $new_task_name,
-    //"new_task_project" => $new_task_project,
-    //"new_task_date"    => $new_task_date,
-    //"errors"           => $errors
+    "projects"          => $projects,
+    "tasks_list"        => $tasks_list,
+    "new_user_email"    => $new_user_email,
+    "new_user_password" => $new_user_password,
+    "new_user_name"     => $new_user_name,
+    "errors"            => $errors
 ]);
 
 $layout = include_template("layout.php", [
--- templates/register_tmp.php
+++ templates/register_tmp.php
@@ -7,29 +7,41 @@
 <main class="content__main">
     <h2 class="content__main-heading">Регистрация аккаунта</h2>
 
-    <form class="form" action="index.html" method="post" autocomplete="off">
+    <form class="form" action="/register.php" method="post" autocomplete="off">
       <div class="form__row">
         <label class="form__label" for="email">E-mail <sup>*</sup></label>
 
-        <input class="form__input form__input--error" type="text" name="email" id="email" value="" placeholder="Введите e-mail">
+        <input class="form__input <?php if ($errors["email"]) { print("form__input--error"); } ?>" type="text" name="email" id="email" value="<?= $new_user_email; ?>" placeholder="Введите e-mail">
 
-        <p class="form__message">E-mail введён некорректно</p>
+        <?php if (array_key_exists("email", $errors)) : ?>
+        <p class="form__message"><?= $errors["email"]; ?></p>
+        <?php endif; ?>
       </div>
 
       <div class="form__row">
         <label class="form__label" for="password">Пароль <sup>*</sup></label>
 
-        <input class="form__input" type="password" name="password" id="password" value="" placeholder="Введите пароль">
+        <input class="form__input <?php if ($errors["password"]) { print("form__input--error"); } ?>" type="password" name="password" id="password" value="<?= $new_user_password; ?>" placeholder="Введите пароль">
+        
+        <?php if (array_key_exists("password", $errors)) : ?>
+        <p class="form__message"><?= $errors["password"]; ?></p>
+        <?php endif; ?>
       </div>
 
       <div class="form__row">
         <label class="form__label" for="name">Имя <sup>*</sup></label>
 
-        <input class="form__input" type="text" name="name" id="name" value="" placeholder="Введите пароль">
+        <input class="form__input <?php if ($errors["name"]) { print("form__input--error"); } ?>" type="text" name="name" id="name" value="<?= $new_user_name; ?>" placeholder="Введите имя">
+        
+        <?php if (array_key_exists("name", $errors)) : ?>
+        <p class="form__message"><?= $errors["name"]; ?></p>
+        <?php endif; ?>
       </div>
 
       <div class="form__row form__row--controls">
+        <?php if (count($errors) !== 0) : ?>
         <p class="error-message">Пожалуйста, исправьте ошибки в форме</p>
+        <?php endif; ?>
 
         <input class="button" type="submit" name="" value="Зарегистрироваться">
       </div>
