b640beb0c685036dc0486866859b40f5427f8c9a
Виталий <wental-@mail.ru>
2019-10-07 22:34:12 +0300

    authentication

---
+++ auth.php
@@ -0,0 +1,83 @@
+<?php
+
+require_once "config.php";
+require_once "helpers.php";
+require_once "database.php";
+
+// показывать или нет выполненные задачи
+$show_complete_tasks = rand(0, 1);
+
+$title = "Вход";
+$user_name = "no name";
+$user = [];
+
+//$user_id, $projects and $tasks_list in database.php now
+
+$email = "";
+$password = "";
+
+if (!empty($_POST["email"])) {
+    $email = htmlspecialchars(trim($_POST["email"]));
+}
+if (!empty($_POST["password"])) {
+    $password = htmlspecialchars(trim($_POST["password"]));
+}
+
+$required_fields = ["email", "password"];
+$errors = [];
+
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    //проверка email на формат
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $errors["email"] = "E-mail введён некорректно";
+    }
+    
+    //проверка на заполнение обязательных полей
+    foreach ($required_fields as $required_field) {
+        if (empty(trim($_POST[$required_field]))) {
+            $errors[$required_field] = "Поле обязательно для заполнения";
+        }
+    }
+    
+    //поиск в БД пользователя с введённым email
+    if (count($errors) === 0) {
+        $user_query = "SELECT * FROM users WHERE email = ?;";
+        $stmt_user = db_get_prepare_stmt($connect, $user_query, [$email]);
+        mysqli_stmt_execute($stmt_user);
+        $stmt_user_result = mysqli_stmt_get_result($stmt_user);
+        $user = mysqli_fetch_assoc($stmt_user_result);
+        
+        if (mysqli_num_rows($stmt_user_result) === 0) {
+            $errors["email"] = "Пользователя с таким E-mail не обнаружено";
+        }
+    }
+    
+    //проверка пароля
+    if (count($errors) === 0) {
+        if (!password_verify($password, $user["password"])) {
+            $errors["password"] = "Введён неправильный пароль";
+        }
+    }
+    
+    //вход на сайт
+    if (count($errors) === 0) {
+        session_start();
+        $_SESSION["user_id"] = $user["id"];
+        $_SESSION["user_name"] = $user["name"];
+        header("Location: /index.php");
+    }
+}
+
+$content = include_template("auth_tmp.php", [
+    "email"     => $email,
+    "password"  => $password,
+    "errors"    => $errors
+]);
+
+$layout = include_template("layout.php", [
+    "title"     => $title,
+    "user_name" => $user_name,
+    "content"   => $content
+]);
+
+print($layout);
---
+++ templates/auth_tmp.php
@@ -0,0 +1,36 @@
+<section class="content__side">
+  <p class="content__side-info">Если у вас уже есть аккаунт, авторизуйтесь на сайте</p>
+
+  <a class="button button--transparent content__side-button" href="/auth.php">Войти</a>
+</section>
+
+<main class="content__main">
+  <h2 class="content__main-heading">Вход на сайт</h2>
+
+  <form class="form" action="" method="post" autocomplete="off">
+    <div class="form__row">
+      <label class="form__label" for="email">E-mail <sup>*</sup></label>
+
+      <input class="form__input <?php if ($errors["email"]) { print("form__input--error"); } ?>" type="text" name="email" id="email" value="<?= $email; ?>" placeholder="Введите e-mail">
+
+      <?php if (array_key_exists("email", $errors)) : ?>
+      <p class="form__message"><?= $errors["email"]; ?></p>
+      <?php endif; ?>
+    </div>
+
+    <div class="form__row">
+      <label class="form__label" for="password">Пароль <sup>*</sup></label>
+
+      <input class="form__input <?php if ($errors["password"]) { print("form__input--error"); } ?>" type="password" name="password" id="password" value="<?= $password; ?>" placeholder="Введите пароль">
+      
+      <?php if (array_key_exists("password", $errors)) : ?>
+      <p class="form__message"><?= $errors["password"]; ?></p>
+      <?php endif; ?>
+    </div>
+
+    <div class="form__row form__row--controls">
+      <input class="button" type="submit" name="" value="Войти">
+    </div>
+  </form>
+
+</main>
